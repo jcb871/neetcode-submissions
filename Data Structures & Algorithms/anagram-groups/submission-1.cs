b19ef8f278c0d05516575bafa5de0ac26ce2bca4
John Joseph <jcb871@gmail.com>
2026-06-18 22:57:55 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> anagramSubList = [];
+        foreach(string str in strs){
+            int[] counts = new int[26];
+            foreach(char c in str) {
+                if(c < 'a' || c > 'z') throw new Exception("Invalid character");
+                int charIndex = c - 'a';
+                Console.WriteLine($"Char {c} = {charIndex}");
+                counts[charIndex]++;
+            }
+            string id = string.Join(",", counts);
+            if(!anagramSubList.TryGetValue(id, out List<string> subList)){
+                subList = [];
+                anagramSubList[id] = subList;
+            }
+            subList.Add(str);
+        }
+        return anagramSubList.Values.ToList();
+    }
+}
