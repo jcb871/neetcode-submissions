0ae879965d05c648fcb6c6e5b13197a908e2c16c
John Joseph <jcb871@gmail.com>
2026-06-21 14:54:35 -0500

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public string Encode(IList<string> strs) {
+        if(strs == null || strs.Count == 0) return string.Empty;
+        StringBuilder encoded = new();
+        foreach(string str in strs){
+            int len = str.Length;
+            encoded.Append(len.ToString("D3") + str);
+        }
+        return encoded.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        if(string.IsNullOrEmpty(s)) return [];
+        List<string> strs = [];
+        int index = 0;
+        while(index <s.Length) {
+            int len = Convert.ToInt16(s.Substring(index, 3));
+            string word = s.Substring(index+3, len);
+            strs.Add(word);
+            index += 3 + len;
+        }
+        return strs;
+   }
+}
