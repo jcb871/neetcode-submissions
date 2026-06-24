5777ec138dfca0430950a57eef395868f847cd39
John Joseph <jcb871@gmail.com>
2026-06-23 22:30:25 -0500

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int maxLen = 0;
+        Dictionary<char, int> found = [];
+        int currLen = 0;
+        int startIndex = 0;
+        for(int i=0; i<s.Length; i++) {
+            char c = s[i];
+            if(found.TryGetValue(c, out int prevIndex) && prevIndex >= startIndex) {
+                startIndex = prevIndex + 1;
+                currLen = i - startIndex + 1;
+            }
+            else {
+                currLen++;
+                if(currLen > maxLen) maxLen = currLen;
+            }
+            found[c] = i;
+        }
+
+        return maxLen;
+    }
+}
