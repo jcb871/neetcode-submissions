19c1e047f6b968ae5e8f5a65f62bddf16a14147a
John Joseph <jcb871@gmail.com>
2026-06-27 13:09:41 -0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new();
+        Dictionary<char, char> opens = new () {
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'},
+        };
+        foreach(char c in s) {
+            if(opens.TryGetValue(c, out char close)) {
+                stack.Push(close);
+            }
+            else if (stack.Count == 0 || stack.Peek() != c) {
+                return false;
+            }
+            else {
+                stack.Pop();
+            }
+        }
+        
+        bool isValid = !stack.Any();
+        return isValid;
+    }
+}
