4a0a5b374a10a7ecdc2049d0910168a3e2b5f822
John Joseph <jcb871@gmail.com>
2026-06-28 17:27:23 -0500

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cs
@@ -0,0 +1,32 @@
+public class MinStack {
+    private Stack<int> _data;
+    private Stack<int> _min;
+    public MinStack() {
+        _data = [];
+        _min = [];
+    }
+    
+    public void Push(int val) {
+        int currentMin = val;
+        if(_min.Count ==0 || _min.Peek() >= val) {
+            _min.Push(val);
+        }
+        _data.Push(val);
+    }
+    
+    public void Pop() {
+        int min = _data.Peek();
+        _data.Pop();        
+        if(min == _min.Peek()){
+            _min.Pop();
+        }
+    }
+    
+    public int Top() {
+        return _data.Peek();
+    }
+    
+    public int GetMin() {
+        return _min.Count > 0 ? _min.Peek() : -1;
+    }
+}
