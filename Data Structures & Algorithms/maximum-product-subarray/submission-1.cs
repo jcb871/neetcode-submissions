f86400262bd87d98e557079f3616b3a2b766d777
John Joseph <jcb871@gmail.com>
2026-09-02 21:40:23 -0500

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MaxProduct(int[] nums) {
+        int n = nums.Length;
+        if(n == 0) return 0;
+
+        int maxProduct = nums[0];
+        int currMax = nums[0], currMin=nums[0];
+        for(int i=1; i<n; i++) {
+            int prevMax = currMax;
+            int prevMin = currMin;
+            int num = nums[i];
+            currMax =  Math.Max(prevMax*num, prevMin*num);
+            currMin =  Math.Min(prevMin*num, prevMax*num);
+            if(currMax < num) currMax = num;
+            if(currMin > num) currMin = num;
+            if(maxProduct < currMax) maxProduct = currMax;
+        }
+
+        return maxProduct;
+    }
+}
