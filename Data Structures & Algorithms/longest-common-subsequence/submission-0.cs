9f37317c704291e6b69f25655b0b21784b07013d
John Joseph <jcb871@gmail.com>
2026-09-07 21:10:49 -0500

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int LongestCommonSubsequence(string text1, string text2) {
+        int m=text1.Length;
+        int n=text2.Length;
+
+        int?[,] memo = new int?[m+1, n+1];
+        return LongestCommonSubsequence(text1, text2, p1:m-1, p2:n-1, memo);
+    }
+
+    private int LongestCommonSubsequence(string text1, string text2, int p1, int p2, int?[,] memo)
+    {
+        if(p1 < 0 || p2 < 0) return 0;
+
+        if(memo[p1+1, p2+1].HasValue) return memo[p1+1, p2+1].Value;
+
+        int result;
+        if(text1[p1] == text2[p2]) {
+            result = 1 + LongestCommonSubsequence(text1, text2, p1-1, p2-1, memo);
+        }
+        else{
+            result = Math.Max(LongestCommonSubsequence(text1, text2, p1, p2-1, memo), LongestCommonSubsequence(text1, text2, p1-1, p2, memo));
+        }
+        memo[p1+1, p2+1] = result;
+
+        return result;
+    }
+}
